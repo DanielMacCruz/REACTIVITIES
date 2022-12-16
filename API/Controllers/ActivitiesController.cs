174761efd16aaf455d35af62af474d88dcc3dee5
DanielMacCruz <danielmaccruz@yahoo.com>
2022-12-16 14:27:38 -0300

    First commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+using Persistence;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+       
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+            
+            
+        }
+
+        [HttpGet] //api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //ap/activities/
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+
+    }
+}
